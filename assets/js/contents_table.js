87b14f32d587f1288b5856668adaf5f9bbfc0cb8
rettouseisama <rettouseisama@gmail.com>
2023-06-18 09:10:38 -0500

    Minor Change

--- assets/js/contents_table.js
+++ assets/js/contents_table.js
@@ -7,13 +7,16 @@ export const CONTENTS_TABLE =
     SECTIONS.forEach((el,i)=> {
         const 
                 SECTION = SECTIONS[i],
-                $MODULE = SECTION.querySelector('h2').textContent,
-                $MODULE_DESCRIPTION = SECTION.querySelector('h2').getAttribute('data-description') || 'No Description'
+                $MODULE = SECTION.querySelector('h3').textContent,
+                $MODULE_DESCRIPTION = SECTION.querySelector('h3').getAttribute('data-description') || 'No Description',
+                $ID = SECTION.querySelector('h3').id
 
         i++
-        $TEMPLATE.querySelector('a').dataset.description = $MODULE_DESCRIPTION
-        $TEMPLATE.querySelector('a').href = `#module__${i}`
+        // $TEMPLATE.querySelector('a').dataset.description = $MODULE_DESCRIPTION
+        //$TEMPLATE.querySelector('a').href = `#module__${i}`
+        $TEMPLATE.querySelector('a').href = `#${$ID}`
         $TEMPLATE.querySelector('a').textContent = `Module ${i}: ${$MODULE}`
+        $TEMPLATE.querySelector('p').textContent = $MODULE_DESCRIPTION
         let $clone = document.importNode($TEMPLATE,true)
         $FRAGMENT.appendChild($clone)
     })
